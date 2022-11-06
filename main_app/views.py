8232db8c522f07939da8fa32cc6e01ea849b6ad6
AWeidenkopf <oliveira.anacalado@gmail.com>
2022-11-06 15:10:41 -0500

    initiate data through sql

--- main_app/views.py
+++ main_app/views.py
@@ -1,25 +1,12 @@
-class Dog:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-dogs = [
-  Dog('Lolo', 'German Shepherd', 'Kinda rude.', 3),
-  Dog('Sachi', 'Poodle', 'Nice big puppy.', 0),
-  Dog('Fancy', 'Chihuahua', 'Angry fluff ball.', 4),
-  Dog('Bonk', 'Labrador', 'Your best friend.', 6)
-]
-
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Dog
 
 def home(request):
-  return HttpResponse('<h1>Hello ૮ ^ ﻌ ^ ა</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def dogs_index(request):
+  dogs = Dog.objects.all()
   return render(request, 'dogs/index.html', { 'dogs': dogs })
