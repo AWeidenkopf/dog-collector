495f5e6e86bd0751138b760a6089b910342fcdc4
AWeidenkopf <oliveira.anacalado@gmail.com>
2022-11-14 09:57:31 -0500

    Config env

--- dogcollector/settings.py
+++ dogcollector/settings.py
@@ -25,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-$x^kzv)**x=#k*vq5_f!b=%*8rrh2-lu#64no@#*y#g44d7_+6'
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
